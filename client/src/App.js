881e677542f46f8cba095adb602ca17bf88746d9
Rodrigo2298 <Rodrigovillarruel98@gmail.com>
2020-09-09 22:20:33 -0300

    se configuro redux y material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -8,23 +8,16 @@ import Landing from './components/Landing/Landing.js';
 function App() {
   return (
     <div className="App">
-       {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-         <p>
-           Edit <code>src/App.js</code> and save to reload.
-         </p>
-         <a
-           className="App-link"
-           href="https://reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer"
-         >
-           Learn React
-         </a>
-       </header> */}
-    <Route  path='/'component={Landing} />
+      <Route  path='/'component={Landing} />
     </div>
   );
 }
+const mapStateToProps = state => {
+  return {
+    algo: state.algo
+  }
+}
 
-export default App;
+
+export default connect(mapStateToProps, null)(App);
+
