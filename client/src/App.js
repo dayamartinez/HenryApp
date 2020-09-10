f89c71769091c77097622cc4bb95bf0752f0d07b
matu2209 <matu.2209@gmail.com>
2020-09-10 11:22:58 -0300

    Se agrega formulario para registro e inicio de sesion

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from "react-redux";
 import {Route} from 'react-router-dom';
+import Register from './components/Landing/Register.js'
 // import {Redirect} from 'react-router';
 import Landing from './components/Landing/Landing.js';
 function App() {
   return (
     <div className="App">
-      <Route  path='/'component={Landing} />
+      <Route  exact path='/'component={Landing} />
+      <Route exact path = '/register' component={Register}/>
     </div>
   );
 }
