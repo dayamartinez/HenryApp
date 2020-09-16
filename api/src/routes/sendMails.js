0f4b779b05c160cb9319208b9cb752683c65d176
unknown <lucia.gentile@gmail.com>
2020-09-16 16:31:14 -0300

    Se agrega el ExcelLoader y se comienzan las pruebas

--- api/src/routes/sendMails.js
+++ api/src/routes/sendMails.js
@@ -8,36 +8,39 @@ app.use(cors());
 
 //enviar mail para invitar a la HenryApp
 app.post('/send-email/:email', (req, res) => {
-    const email = req.body.email;
+    const emails = req.body.emails;
 
-    console.log(req.body);
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
-    },
-    tls: {
-        rejectUnauthorized: false
-      }
-})
-
-const mailOptions = {
-    from: "Remitente",
-    to: email,
-    subject: "Enviado desde HenryApp",
-    text: "Bienvenido a HenryApp para registrarse haga click en el siguiente link"
-}
-
-    transporter.sendMail(mailOptions, (err, info) => {
-        if(err){
-            res.status(500).send(err)
-        } else {
-            console.log("Email enviado")
-            res.status(200).json(req.body)
+    emails.map(email => {
+        console.log(req.body);
+        const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        },
+        tls: {
+            rejectUnauthorized: false
         }
+        })
+
+        const mailOptions = {
+            from: "Remitente",
+            to: email,
+            subject: "Enviado desde HenryApp",
+            text: "Bienvenido a HenryApp!! Para registrarse haga click en el siguiente link "
+        }
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if(err){
+                res.status(500).send(err)
+            } 
+            else {
+                console.log("Email enviado")
+                res.status(200).json(req.body)
+            }
+        })
     })
 });
 
