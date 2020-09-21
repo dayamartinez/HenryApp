a8bd926f942b2d74c24cecb6cae89053bb500c49
Rodrigo2298 <Rodrigovillarruel98@gmail.com>
2020-09-21 14:58:23 -0300

    se agrego el fondo al landing y algo de estilo

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import Login from "./Login.js" 
+import {makeStyles, Grid, Container} from '@material-ui/core';
+import clsx from 'clsx';
+import fondo from "../../images/Fondo.png"
+const useStyles = makeStyles((theme) => ({
+  fondo:{
+   backgroundImage:`url(${fondo})` ,
+   backgroundPosition: "center",
+   backgroundRepeat: "no-repeat",
+   backgroundSize: "cover",
+   height: "100vh",
+  },
+  titleBack:{
+    top:25,
+    backgroundColor: theme.palette.primary.main,
+    height: 25,
+    position:"relative",
+    zIndex:0,
+    width:340,
+  },
+  title:{
+    // backgroundColor: theme.palette.primary.main,<h1>Ingrese su email con el que aplico.</h1>
+    zIndex:1,
+    position:"relative",
+    top:-20,
+    left:30,
+  }
+}))
 
 export  default function Landing() {
+  const s = useStyles();
+  const fixedHeightChart = clsx(s.paper, s.fixedHeightChart);
+
 return (
-  <div>
-    <div>
-      <h1>Bienvenido a HenryApp</h1>
-      <h1>Ingrese su email con el que aplico.</h1>
-    </div>
-    {/* llamo al componente Login importado */}
-    <div>
-      <Login/>
-    </div>
+  <div className={s.fondo}>
+    <Container>
+      <Grid>
+        <Grid item className={s.elemento} xs={12}>
+          <div className={s.titleBack}>
+            <h2 className= {s.title} >
+              Bienvenido a HenryApp
+            </h2>
+          </div>
+          
+        </Grid>
+        
+        {/* llamo al componente Login importado */}
+        <Grid item xs={12}>
+          <Login/>
+        </Grid>
+      </Grid>
+    </Container>
   </div>
 )
 }
