6db570d954ec207fa361dcf49441d6505100f1db
unknown <Dayamar@Dayamar-PC>
2020-09-28 20:33:06 -0300

    resolv

--- client/src/components/Cohort/AllCohorts.jsx
+++ client/src/components/Cohort/AllCohorts.jsx
@@ -23,76 +23,6 @@ export function AllCohorts({getCohorts,style}){
     getCohorts()
     .then(data => setCohorts(data.payload))    
   }, [])
-<<<<<<< HEAD
-  var data
-  
-  const history = useHistory()
-
-  if(cohorts){
-    data = cohorts && cohorts.map(cohort => 
-      ({
-        cohorte: cohort.name,
-        inicio: cohort.startDate,
-        sobre: cohort.about,
-        alumnos: cohort.usuarios.length,
-        id: cohort.id,
-        instructor: cohort.staffs
-      })
-    )
-    console.log(data)
-  }
-  console.log(cohorts)
-  return (
-    <div style={style}>  
-      {cohorts && cohorts.length === 0 ? (
-        <div>
-          <h4>
-            {' '}
-            No hay cohortes para mostrar
-          </h4>
-        </div>
-      ) : (
-        <TableContainer>
-          <Table>
-            <TableHead style={{backgroundColor:grey[900]}}>
-              <TableRow  >
-                <TableCell style={yellowText} >Cohorte</TableCell>
-                <TableCell style={yellowText} >Fecha de inicio</TableCell>
-                <TableCell style={yellowText} >Instructor</TableCell>
-                <TableCell style={yellowText} >Alumnos</TableCell>
-                <TableCell style={yellowText} ></TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data && data.map(celda => (
-                <TableRow>
-                  {console.log(celda)}
-                  <TableCell>
-                    <Link 
-                      href={"/cohortDetail/"+celda.id} 
-                      color="inherit" 
-                      underline="none">
-                        {celda.cohorte}
-                    </Link>
-                  </TableCell>
-
-                  <TableCell>{celda.inicio}</TableCell>
-
-                  <TableCell>{celda.instructor[0].name + ' ' + celda.instructor[0].lastName }</TableCell>
-
-                  <TableCell>{celda.alumnos}</TableCell>
-                  
-                  <TableCell><button type="button" onClick={() => history.push(`/cohortDetail/${celda.id}`)} class="btn btn-outline-dark" >Detalles</button> </TableCell>
-
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        )
-      }
-    </div>
-=======
 
   return (
   
@@ -127,7 +57,6 @@ export function AllCohorts({getCohorts,style}){
           </tbody>
         </table>
       </div>
->>>>>>> master
     )
 }
 
