c94fe7deb325e31099c3547a17bd4059e990c7e9
Rodrigo2298 <Rodrigovillarruel98@gmail.com>
2020-09-29 13:44:26 -0300

    un poco de estilo

--- client/src/components/Groups/FormGroup.js
+++ client/src/components/Groups/FormGroup.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { CssBaseline,Button,TextField,Typography,Container,Grid,Box } from '@material-ui/core';
+import { CssBaseline,Button,TextField,Typography,Container,Grid,Box, Input } from '@material-ui/core';
 import { makeStyles} from '@material-ui/core';
 import { connect } from 'react-redux'
 import axios from "axios"
@@ -11,151 +11,98 @@ import ExcelLoader from '../Cohort/ExcelLoader';
 /* componente de creacion de grupos  */
 
   const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    form: {
-      width: '100%', 
-      marginTop: theme.spacing(1),
-    },
     submit: {
       margin: theme.spacing(2, 0, 0),
       marginButton: theme.spacing(2)
     },
-    delete: {
-      backgroundColor: 'darkgray',
-      color: 'white',
-      marginBorder: '0px'     
-    },
-    fondo1:{
-      backgroundColor:theme.palette.primary
+    inputGroup:{
+      fontSize:"1.5rem",
+      right:1
     }
   }));
   
-  export function FormGroup({ getPm,pms, addGroup, emails}) {
+  export function FormGroup({ getPm,pms, addGroup, emails, style}) {
 
-    const history = useHistory()
-    const classes = useStyles();
-    const [pm,setPm] = useState(pms)
-    const [groups,setGroups] = useState([])
-    const [cohort, setCohort] = useState({})
-    const [input, setInput]= useState({
-      name:'',
-      startDate: ''
-    });
-    
-    var aux
-    useEffect(()=>{
-      aux = pm.filter(p=> !p.groupId)
-        getPm().then(res=>{
-          console.log(res.data)
-          setPm(
-            aux
-            )
-          })
-        console.log(pms)
-        axios.get("http://localhost:3001/cohort")
-        .then(res=>{
-          console.log(res.data)
-          setCohort(
-            res.data[res.data.length-1]
-            )
-          })
-          .catch(err =>{console.log(err)})          
-        },[])
-        
-    const handleInputChange = function(e) {
-      console.log(cohort)
-      const g=Number(e.target.value)
-      if (g > pm.length){
-        alert("error")
-        e.target.value = g-1
-      }else if (g <= 0){
-        alert("error")
-        e.target.value = 1
-      } else{
-        setInput({
-          ...input,
-          [e.target.name]:e.target.value
+  const history = useHistory()
+  const classes = useStyles();
+  const [pm,setPm] = useState(pms)
+  const [groups,setGroups] = useState([])
+  const [cohort, setCohort] = useState({})
+  const [input, setInput]= useState({
+    name:'',
+    startDate: ''
+  });
+  
+  var aux
+  useEffect(()=>{
+    aux = pm.filter(p=> !p.groupId)
+      getPm().then(res=>{
+        setPm(
+          aux
+          )
         })
-      }
+      axios.get("http://localhost:3001/cohort")
+      .then(res=>{
+        setCohort(
+          res.data[res.data.length-1]
+          )
+        })
+        .catch(err =>{console.log(err)})          
+      },[input])
+      
+  const handleInputChange = function(e) {
+    console.log(cohort)
+    const g=Number(e.target.value)
+    if (g > pm.length){
+      e.target.value = g-1
+    }else if (g <= 0){
+      e.target.value = 1
+    } else{
+      setInput({
+        ...input,
+        [e.target.name]:e.target.value
+      })
     }
-
-    const handleSubmit = function (e) {
-        e.preventDefault()
-        addGroup(cohort.id,input.grupos)
-        axios.put("http://localhost:3001/pm/setGroup/"+cohort.id)
-    }
-
-    return (
-      <Container component="main" maxWidth="xs">
-        <Grid container>
-          <Grid item >
-            <Box className={classes.fondo1}>
-              <Typography>
-                Cohorte
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item>
-            <Box>
-            <Typography>
-                Alumnos Totales del Cohorte:{emails.length}
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item>
-            <Box>
-            <Typography>
-                Projects Managers Disponibles:{pm.length}
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item>
-            <Box>
-              <Typography>
-                Ingresar Cantidad de Grupos a Crear
-              </Typography>
-            </Box>
-            <Box>
-              <TextField type="number" name="grupos" onChange={(e) => handleInputChange(e)}/>
-            </Box>
-          </Grid>
-          <Grid item>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={(e) => handleSubmit(e)}
-          >
-              Crear
-          </Button>
-          </Grid>
-          <Grid item>
-            <Button
-            fullWidth
-            variant="contained"
-            className={classes.submit}
-            onClick={() => history.replace('/admin') }
-          >
-              Cancelar
-          </Button>
-          </Grid>
-          
-        </Grid>
-      </Container>
-    )
   }
+
+  const handleSubmit = function (e) {
+      e.preventDefault()
+      addGroup(cohort.id,input.grupos)
+      axios.put("http://localhost:3001/pm/setGroup/"+cohort.id)
+  }
+
+  return (
+    <div style={style}>  
+    <Container component="main" maxWidth="sm" style={{margin:"auto",backgroundColor:"rgb(220,220,220)"}}>
+      <Typography component="h1" variant="h4" align="center">  Crear Grupos </Typography>
+      <Grid container style={{margin:"auto"}}>
+        <Grid item xs={6}> <Typography component="h1" variant="h5">  Cohorte: </Typography> </Grid>
+        <Grid item xs={6}> <Typography component="h1" variant="h5" align="right">  {cohort.name?cohort.name:"..."} </Typography> </Grid>
+        <Grid container>
+          <Grid item xs={6}> <Typography component="h1" variant="h5"> Alumnos Totales </Typography> </Grid>
+          <Grid item xs={6}> <Typography component="h1" variant="h5" align="right"> {cohort.usuarios?cohort.usuarios.length:"..."} </Typography> </Grid>
+          <Grid item xs={6}> <Typography component="h1" variant="h5"> PMs Disponibles </Typography> </Grid>
+          <Grid item xs={6}> <Typography component="h1" variant="h5" align="right"> {pm.length} </Typography> </Grid>
+        </Grid>
+        <Grid container style={{justifyContent:"space-between"}}>
+          <Grid item xs={6}> <Typography component="h1" variant="h5"> Cuantos Grupos?</Typography> </Grid>
+          <Grid item xs={2} sm={1}> <Input type="number" className={classes.inputGroup} name="grupos" defaultValue={1} onChange={(e) => handleInputChange(e)}/> </Grid>
+        </Grid>
+        <Grid style={{margin:"auto"}} item xs={6}>
+          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit} onClick={(e) => handleSubmit(e)} >
+            Crear
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+    </div>
+  )
+}
 
   const mapStateToProps = (state) => {
     return{
       emails: state.cohort.emails,
-      pms: state.pm.pms
+      pms: state.pm.pms,
     }
   }
  
